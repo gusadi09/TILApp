dc7d6b74f0f22b9ceb49039437b3e984a5ff8f3f
Agus Adi <gusadi872@gmail.com>
2022-10-11 16:08:05 +0700

    make get all user list and get single user list

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -13,11 +13,25 @@ struct UsersController: RouteCollection {
 		let userRoutes = routes.grouped("api", "users")
 
 		userRoutes.post(use: createHandler)
+		userRoutes.get(use: getAllHandler)
+		userRoutes.get(":userID", use: getSingleHandler)
 	}
 
 	func createHandler(_ req: Request) throws -> EventLoopFuture<User> {
 		let user = try req.content.decode(User.self)
 
 		return user.save(on: req.db).map { user }
+	}
+
+	func getAllHandler(_ req: Request) -> EventLoopFuture<[User]> {
+		User.query(on: req.db).all()
+	}
+
+	func getSingleHandler(_ req: Request) -> EventLoopFuture<User> {
+		User.find(
+			req.parameters.get("userID"),
+			on: req.db
+		)
+		.unwrap(or: Abort(.notFound))
 	}
 }
