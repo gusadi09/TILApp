6ff1317fae1fdb2b8619f35d1b7a8b51a290b7d6
Agus Adi <gusadi872@gmail.com>
2022-10-11 15:12:11 +0700

    make update handler on controller

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -14,6 +14,7 @@ struct AcronymsController: RouteCollection {
 
 		acronymsGroupRoute.get(use: getAllHandler)
 		acronymsGroupRoute.post(use: createHandler)
+		acronymsGroupRoute.put(":acronymID", use: updateHandler)
 	}
 
 	func getAllHandler(_ req: Request) -> EventLoopFuture<[Acronym]> {
@@ -24,5 +25,21 @@ struct AcronymsController: RouteCollection {
 		let acronym = try req.content.decode(Acronym.self)
 
 		return acronym.save(on: req.db).map { acronym }
+	}
+
+	func updateHandler(_ req: Request) throws -> EventLoopFuture<Acronym> {
+		let updateAcronym = try req.content.decode(Acronym.self)
+
+		return Acronym.find(
+			req.parameters.get("acronymID"),
+			on: req.db
+		)
+		.unwrap(or: Abort(.notFound))
+		.flatMap { acronym in
+			acronym.short = updateAcronym.short
+			acronym.long = updateAcronym.long
+
+			return acronym.save(on: req.db).map { acronym }
+		}
 	}
 }
